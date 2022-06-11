6b1ea8ff5d7bf87d5ed371993d0b1096482fa737
Alkylaid <alkylaid@gmail.com>
2022-06-11 01:34:19 -0500

    Add increment opacity ability

--- javascript.js
+++ javascript.js
@@ -6,8 +6,16 @@ const resetButton = document.querySelector('.resetButton');
 const eraserButton = document.querySelector('.eraserButton');
 const randomButton = document.querySelector('.randomButton');
 const colorButton = document.querySelector('.colorButton');
+const incrementButton = document.querySelector('.incrementButton');
 let mode;
+let mouseDown = false;
+let increment = false;
+
+
 changeMode(`color`);
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 output.innerHTML = slider.value + "  x " + slider.value;
 
@@ -25,21 +33,32 @@ function createGrid(size) {
 
 function clearGrid() {
     while (grid.lastChild) {
-    grid.removeChild(grid.lastChild);
+        grid.removeChild(grid.lastChild);
     }
 }
 
-function hover() {
+function hover(e) {
     const squareHover = document.querySelectorAll('.square');
 
     squareHover.forEach((square) => {
-        
+        square.style.opacity = 0.1;
         square.addEventListener('mouseover', () => {
-            
-            if (mode == 'color'){
+            if (!mouseDown) {
+                return;
+            }
+            if (increment) {
+                if (square.style.opacity < 1.0) {
+                square.style.opacity = parseFloat(square.style.opacity) + 0.10; 
+                console.log(square.style.opacity);}
+
+            } else {
+                square.style.opacity = 1.0;
+            }
+            if (mode == 'color') {             
                 square.style.backgroundColor = document.getElementById('colorPicker').value;
-            } else if (mode == 'random'){
-            square.style.backgroundColor = generateRandomColor();}
+            } else if (mode == 'random') {
+                square.style.backgroundColor = generateRandomColor();
+            }
             else if (mode == 'eraser') {
                 square.style.backgroundColor = '#ffffff';
             }
@@ -59,8 +78,8 @@ function reset() {
     createGrid(slider.value);
     hover();
     output.innerHTML = slider.value + "  x " + slider.value;
-    
-    
+
+
 }
 
 function generateRandomColor() {
@@ -72,7 +91,7 @@ function generateRandomColor() {
 
 }
 
-slider.oninput = function() {
+slider.oninput = function () {
     output.innerHTML = this.value + "  x " + this.value;
     clearGrid();
     createGrid(this.value);
@@ -91,6 +110,24 @@ randomButton.onclick = () => {
 eraserButton.onclick = () => {
     changeMode('eraser');
 }
+
+incrementButton.onclick = () => {
+    if (increment) {
+        incrementButton.classList.remove('button-active');
+        increment = false;
+    } else {
+    increment = true;
+    incrementButton.classList.add('button-active');
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        if(square.style.opacity < 0.1) {
+            square.style.opacity = 0.1;
+        }
+    });
+    }
+}
+    
+
 
 function changeMode(newMode) {
     mode = newMode;
